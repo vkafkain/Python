a80455377815fff90f45b96eb067ba860d1c160e
victor.lopez <victor.lopez@AM-BCN-POR-717>
2023-06-29 12:49:10 +0200

    finish interpreter

---
+++ ProblemSet1/interpreter/interpreter.py
@@ -0,0 +1,25 @@
+import decimal
+
+userInput = input("Expression: ")
+
+x, y, z = userInput.split(" ")
+
+if y == "+":
+    result = (float(x) + float(z))
+    rounded = round(result, 1)
+    print(rounded)
+
+elif y == "-":
+    result = (float(x) - float(z))
+    rounded = round(result, 1)
+    print(rounded)
+
+elif y == "*":
+    result = (float(x) * float(z))
+    rounded = round(result, 1)
+    print(rounded)
+
+elif y == "/":
+    result = (float(x) / float(z))
+    rounded = round(result, 1)
+    print(rounded)
