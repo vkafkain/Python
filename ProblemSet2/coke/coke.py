1ff3f415458b25d8f66613224ddb54f8c01a7cbb
victor.lopez <victor.lopez@AM-BCN-POR-717>
2023-07-05 09:21:34 +0200

    finish coke

--- ProblemSet2/coke/coke.py
+++ ProblemSet2/coke/coke.py
@@ -1,3 +1,16 @@
 def main():
+    
     total_prize = 50
-    print(f"Amount Due: {total_prize}")
+    while True:
+        print(f"Amount Due: {total_prize}")
+
+        coins = int(input("Insert Coin:"))
+        total_prize = total_prize - coins
+        
+        if total_prize > 0:
+            continue
+        else:
+            print(f"Change Owed: {total_prize}")
+            break
+
+main()
